693a2fed7969e69963a03d6720b7ba27bca6209c
Ramchandra4034 <84640555+Ramchandra4034@users.noreply.github.com>
2021-07-01 21:18:02 +0530

    Update main.c

--- main.c
+++ main.c
@@ -49,31 +49,7 @@ void test_ssort() {
 int main(void) {
   test_swap_max();
   test_ssort();
-void swap_max(int arr[],int l,int n){
+
  
-  int count = arr[n];
-  int sum = n;
-  int foo = arr[n];
-  for(int x =n+1;x<l;x++){
-    if(count<arr[x]){
-      count=arr[x];
-      sum = x;
-      }
-  }
-  arr[n]=arr[sum];
-  arr[sum]=foo;
-  
-  
-  
-  
-  }
- void ssort(int arr[],int l){
-   
-   for(int x=0;x<l;x++){
-     swap_max(arr,l,x);
-     
-   }
-   
- }
   }
 
